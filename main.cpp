1dc1aa9b52f469408bba97619a7c86b5a15ebf07
Saksham <apple@apples-MacBook-Pro.local>
2024-03-30 18:09:00 +0530

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <hpx/hpx_main.hpp>
+#include<hpx/iostream.hpp>
+using namespace hpx;
+int main() {
+    cout<<"Hello world!\n"<<std::flush;
+    float f=2.5*3.5;
+    cout<<f;
+    return 0;
+}
